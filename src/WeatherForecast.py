ae2910fb040998d83f9ba8b4cd022c046da6f4f0
Jonathon Vrankul <jvra0001@student.monash.edu>
2019-10-11 15:52:41 +1100

    Add all args to parser

--- src/WeatherForecast.py
+++ src/WeatherForecast.py
@@ -1,14 +1,50 @@
 import argparse
+import sys
 
-parser = argparse.ArgumentParser(description='Add some integers.')
 
-parser.add_argument('api', metavar='api', type=int, nargs='+',
-                    help='api key')
+def parse_args(args):
+    parser = argparse.ArgumentParser(
+        description='A simple weather forecasting program.')
 
-parser.add_argument('--sum', action='store_const',
-                    const=sum, default=max,
-                    help='sum the integers (default: find the max)')
+    parser.add_argument('-api', required=True,
+                        help='openweather api key')
 
-args = parser.parse_args()
+    parser.add_argument('-city',
+                        help='city name')
 
-print(args.sum(args.integers))
+    parser.add_argument('-gc',
+                        help='geographic coordinates')
+
+    parser.add_argument('-z',
+                        help='zip code')
+
+    parser.add_argument('-time',
+                        help='time')
+
+    parser.add_argument('-temp',
+                        help='temperature (celsius, fahrenheit) default is celsius')
+
+    parser.add_argument('-pressure',
+                        help='includes pressure information')
+
+    parser.add_argument('-cloud',
+                        help='includes cloud information')
+
+    parser.add_argument('-humidity',
+                        help='includes humidity information')
+
+    parser.add_argument('-wind',
+                        help='includes wind information')
+
+    parser.add_argument('-sunset',
+                        help='includes sunset time')
+
+    parser.add_argument('-sunrise',
+                        help='includes sunrise time')
+    # ...Create your parser as you like...
+    return parser.parse_args(args)
+
+
+if __name__ == "__main__":
+    print('WeatherForecast ran with ' + str(len(sys.argv) - 1) + ' args.')
+    parser = parse_args(sys.argv[1:])
