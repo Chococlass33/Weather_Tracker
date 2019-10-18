ef7cbeeba94ef5e07b794930f719c367cb2e39e8
Chee Chin Chan <ccha0048@student.monash.edu>
2019-10-18 17:00:13 +1100

    fixed test, added to strategy

--- WeatherForecast.spec.py
+++ WeatherForecast.spec.py
@@ -86,6 +86,6 @@ class TestWeatherForecast(unittest.TestCase):
         runner = CliRunner();
         self.maxDiff = None
         result = runner.invoke(main, ['--api', '3c3e7a56277f4e3239deba8785391d1a', '--city', 'Melbourne', '--temp','celsius','--time','--pressure','--cloud','--humidity','--wind','--sunrise','--sunset'])
-        self.assertEqual(result.output[:-2], 'On 2019-10-15 at ' + datetime.datetime.fromtimestamp(mock_request()['dt']).strftime('%H:%M:%S')+' The temperature is 286.65 degrees celsius,  Air pressure is 1014 hPa. Humidity is 82%. Cloud coverage is 75%. Sunrise time '+datetime.datetime.fromtimestamp(mock_request()['sys']['sunrise']).strftime('%H:%M:%S')+'. Sunset time '+datetime.datetime.fromtimestamp(mock_request()['sys']['sunset']).strftime('%H:%M:%S')+'. Wind speed is 3.1 from 200 degrees')
+        self.assertEqual(result.output[:-2], 'On 2019-10-15 at ' + datetime.datetime.fromtimestamp(mock_request()['dt']).strftime('%H:%M:%S')+', The temperature is 286.65 degrees celsius,  Air pressure is 1014 hPa. Humidity is 82%. Cloud coverage is 75%. Sunrise time '+datetime.datetime.fromtimestamp(mock_request()['sys']['sunrise']).strftime('%H:%M:%S')+'. Sunset time '+datetime.datetime.fromtimestamp(mock_request()['sys']['sunset']).strftime('%H:%M:%S')+'. Wind speed is 3.1 from 200 degrees')
 if __name__ == '__main__':
     unittest.main()
